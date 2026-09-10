ef5fd5d6d173dff0250daf877658d9cdd53db830
Zinain <ailzar@bk.ru>
2026-09-10 14:38:30 +1000

    Added SAM mock integration tests

--- tests/api.integration.test.ts
+++ tests/api.integration.test.ts
@@ -1,14 +1,14 @@
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = process.env.API_URL ?? "http://127.0.0.1:3000";
 
 describe("API Gateway integration", () => {
 
     test("GET /hello returns greeting", async () => {
 
-        if (!API_BASE_URL) {
-            throw new Error(
-                "API_BASE_URL environment variable is missing"
-            );
-        }
+        // if (!API_BASE_URL) {
+        //     throw new Error(
+        //         "API_BASE_URL environment variable is missing"
+        //     );
+        // }
 
         const response = await fetch(
             `${API_BASE_URL}/hello?name=Ilzar`
