b6fa867784c82d34ae9f0ea4206c1dce77b9019d
Zinain <ailzar@bk.ru>
2026-09-10 14:51:01 +1000

    Minor issues

--- tests/api.integration.test.ts
+++ tests/api.integration.test.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.API_URL ?? "http://127.0.0.1:3000";
+const API_BASE_URL = process.env.API_BASE_URL ?? "http://127.0.0.1:3000";
 
 describe("API Gateway integration", () => {
 
